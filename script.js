25318aca95cb1a5761dbe7832d595b8adc1d33ac
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-07-11 15:15:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+   // Trim leading and trailing whitespace
+    str = str.trim();
+    
+    // Find the index of the first space
+    let index = str.indexOf(' ');
+    
+    // If no space is found, return the entire string
+    if (index === -1) {
+        return str;
+    }
+    
+    // Otherwise, return the substring from the start to the first space
+    return str.substring(0, index);
 }
 
 // Do not change the code below
